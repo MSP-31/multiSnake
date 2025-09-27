7a78101d441c248158295651edb086b3109c2dbf
DESKTOP-CEKNQTC <jhss9747@gmail.com>
2025-09-27 18:59:45 +0900

    init: " 초기 설정"

---
+++ src/pages/Game.tsx
@@ -0,0 +1,4 @@
+// pages/Game.tsx
+export default function Game() {
+    return <h1>게임 화면입니다</h1>;
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import {useNavigate} from "react-router-dom";
+import StartButton from "../components/StartButton";
+
+export default function Home() {
+    const navigate = useNavigate();
+    const handleStart = () => {
+        navigate("/game");
+    };
+
+    return (
+        <div>
+            <h1>홈 화면</h1>
+            <StartButton onClick={handleStart} />
+        </div>
+    );
+}
